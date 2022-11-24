de56474d9be932f0573eb9871ea71b8b750202cf
Salgado, Kim Darell R <118818974+kimsalgado02@users.noreply.github.com>
2022-11-24 16:21:54 +0800

    Update nature.m

--- Midterms/nature.m
+++ Midterms/nature.m
@@ -2,32 +2,38 @@ clc;
 clf;
 clear all;
 close all;
+
 pkg load image;
 
-image_2=imread('nature.jpg');
-whos image_2
-#convert original image to red image.
-img_red=image_2;
-img_red(:,:,2)=0;
-img_red(:,:,3)=0;
+% Read the image information
+nature=imread('C:\CPEN111\nature.jpg');
+whos nature;
+imwrite(nature,'nature.png');
 
-#convert original image to green image.
-img_green=image_2;
-img_green(:,:,1)=0;
-img_green(:,:,3)=0;
+% grayscale
+grayscale_nature = rgb2gray(nature);
+whos grayscale_nature;
 
-#convert original image to blue image.
-img_blue=image_2;
-img_blue(:,:,1)=0;
-img_blue(:,:,2)=0;
+% Intensify image in red
+nature_red=nature;
+nature_red(:,:,2)=0;
+nature_red(:,:,3)=0;
+imwrite(nature_red,'nature_red.png');
 
-#convert jpg image to png image.
-imwrite(image_2,'nature.png');
-imwrite(img_red,'nature_red.png');
-imwrite(img_green,'nature_green.png');
-imwrite(img_blue,'nature_blue.png');
+% Intensify image in green
+nature_green=nature;
+nature_green(:,:,1)=0;
+nature_green(:,:,3)=0;
+imwrite(nature_green,'nature_green.png');
 
-subplot(2,2,1),imshow(image_2),title('original image');%print original image.
-subplot(2,2,2),imshow(img_red),title('red  image');%print gray image.
-subplot(2,2,3),imshow(img_green),title('green image');%print gray image.
-subplot(2,2,4),imshow(img_blue),title('blue image');%print gray image.
+% Intensify image in blue
+nature_blue=nature;
+nature_blue(:,:,1)=0;
+nature_blue(:,:,2)=0;
+imwrite(nature_blue,'nature_blue.png');
+
+
+subplot(2,2,1),imshow(nature),title('original image');%print original image.
+subplot(2,2,2),imshow(nature_red),title('red  image');%print gray image.
+subplot(2,2,3),imshow(nature_green),title('green image');%print gray image.
+subplot(2,2,4),imshow(nature_blue),title('blue image');%print gray image.
